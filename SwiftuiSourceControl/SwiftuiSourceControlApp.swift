da6f025a7cc8d28c07c76eddfd0bbcdcf12c94cb
Leo <hbuliuyu2020@gmail.com>
2024-05-25 10:24:37 +0700

    Initial Commit

---
+++ SwiftuiSourceControl/SwiftuiSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiSourceControlApp.swift
+//  SwiftuiSourceControl
+//
+//  Created by Leo on 2024/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
